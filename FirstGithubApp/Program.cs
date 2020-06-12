60a456c1ecd33063469dc20fe603b87d957a04b2
Joseph Bakata <jprogrammer@protonmail.com>
2020-06-12 18:36:20 -0400

    trying to get this to work

---
+++ FirstGithubApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstGithubApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Github App!");
+        }
+    }
+}
